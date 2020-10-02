14246d093b287c47e247e985dbfa15b72d1449f6
WaiLin <leon.wai@live.com>
2020-10-02 16:32:11 +0630

    Updates

---
+++ precache-manifest.3b0d6d588b3648b60e7b4ea64f772366.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e7288f5f75a43be160aa846afed73c8",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "ad68429ffddefcec4fb4",
+    "url": "/home/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "3a36780ad783b7c7fb7a",
+    "url": "/home/static/css/main.a3c6112f.chunk.css"
+  },
+  {
+    "revision": "ad68429ffddefcec4fb4",
+    "url": "/home/static/js/2.1a85cb42.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/home/static/js/2.1a85cb42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a36780ad783b7c7fb7a",
+    "url": "/home/static/js/main.62b69c57.chunk.js"
+  },
+  {
+    "revision": "327519257ae83559732d",
+    "url": "/home/static/js/runtime-main.1ebb12cf.js"
+  }
+]);
